4c52a4d3b47b5b65c5a0e9c540ba2f0791c6822f
Lauri Ajanki <lauri.ajanki@gmail.com>
2021-01-04 22:55:13 +0200

    Separate CPU poller for Windows.

---
+++ pubsub_utils/hw_stats.py
@@ -0,0 +1,93 @@
+import logging
+
+import wmi
+import psutil
+import pynvml
+import gpustat
+
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level="INFO")
+
+
+# GPU statistics is based on NVIDIA library, disable if unable to load nvml
+IGNORE_GPU = False
+try:
+    pynvml.nvmlInit()
+except pynvml.NVMLError_LibraryNotFound:
+    logging.warning("NVML library not found, disabling GPU statistics")
+    IGNORE_GPU = True
+
+
+
+def get_ram_info():
+    mem = psutil.virtual_memory()
+    return {
+        "total": int(mem.total / 1000**2),
+        "used": int(mem.used / 1000**2),
+        "available": int(mem.available / 1000**2)
+    }
+
+
+def get_gpu_info():
+    if IGNORE_GPU:
+        stats = {
+            "memory.used": 0,
+            "memory.total": 1, # non zero value to avoid division by zero
+            "utilization": 0,
+            "temperature": 0
+        }
+
+    else:
+        gpustats = gpustat.GPUStatCollection.new_query().jsonify()
+        stats = {
+            "memory.used": gpustats["gpus"][0]["memory.used"],
+            "memory.total": gpustats["gpus"][0]["memory.total"],
+            "utilization": gpustats["gpus"][0]["utilization.gpu"],
+            "temperature": gpustats["gpus"][0]["temperature.gpu"]
+        }
+
+    return stats
+
+
+def _get_cpu_info_psutil():
+    """Fetch current CPU core temperature, frequnces and loads using psutil."""
+    cpu_temps = psutil.sensors_temperatures()
+
+    temperatures = [item.current for item in cpu_temps["coretemp"] if "Core" in item.label]
+    frequences = psutil.cpu_freq(percpu=True)
+    load = psutil.cpu_percent(percpu=True)
+
+    return {"utilization": load, "freq": frequences, "temperature": temperatures }
+
+def _get_cpu_info_wmi():
+    """Fetch current CPU core temperature, frequnces and loads using wmi and OpenHardwareMonitor
+    psutil has limted functionality in Windows. Windows Management Instrumentation is used instead.
+    Requires https://openhardwaremonitor.org/ running in the background.
+    http://timgolden.me.uk/python/wmi/index.html.
+    https://openhardwaremonitor.org/
+    """
+    w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+    ohm_infos = w.Sensor()
+
+    temperatures = []
+    frequences = []
+    load = []
+    for sensor in ohm_infos:
+        if sensor.SensorType == "Temperature":
+            if sensor.Name.startswith("CPU Core #"):
+                temperatures.append((sensor.Value, int(sensor.Name[-1])))
+
+        elif sensor.SensorType == "Clock":
+            if sensor.Name.startswith("CPU Core #"):
+                frequences.append((sensor.Value, int(sensor.Name[-1])))
+
+        elif sensor.SensorType == "Load":
+            if sensor.Name.startswith("CPU Core #"):
+                load.append((sensor.Value, int(sensor.Name[-1])))
+
+    temperatures = [int(t[0]) for t in sorted(temperatures, key=lambda token: token[1])]
+    frequences = [int(t[0]) for t in sorted(frequences, key=lambda token: token[1])]
+    load = [int(t[0]) for t in sorted(load, key=lambda token: token[1])]
+
+    return {"utilization": load, "freq": frequences, "temperature": temperatures }
+
--- pubsub_utils/publisher.py
+++ pubsub_utils/publisher.py
@@ -1,14 +1,12 @@
+import os
 import json
 import threading
-import argparse
 import logging
 
-import psutil
-import gpustat
 from google.cloud import pubsub_v1
-import pynvml
 
 import pubsub_utils
+import hw_stats
 
 
 publisher = pubsub_v1.PublisherClient()
@@ -18,64 +16,22 @@ topic_path = publisher.topic_path(pubsub_utils.PROJECT_ID, pubsub_utils.TOPIC_ID
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level="INFO")
 
-# GPU statistics is based on NVIDIA library, disable if unable to load nvml
-IGNORE_GPU = False
-try:
-    pynvml.nvmlInit()
-except pynvml.NVMLError_LibraryNotFound:
-    logging.warning("NVML library not found, disabling GPU statistics")
-    IGNORE_GPU = True
 
 
 def get_stats():
     """Gather various CPU, GPU and RAM statistics to a dict to be sent
     as pubsub message.
     """
-    stats = {
-        "cpu": {"utilization": [], "freq": [], "temperature": []},
-        "ram": {},
-        "gpu": {}
-    }
-
-    # CPU statistics per core
-    cpu_percent = psutil.cpu_percent(percpu=True)
-    cpu_freq = psutil.cpu_freq(percpu=True)
-
-    cpu_temps = psutil.sensors_temperatures()
-    core_temps = [item.current for item in cpu_temps["coretemp"] if "Core" in item.label]
-
-    for i, core in enumerate(cpu_percent):
-        stats["cpu"]["utilization"].append(int(cpu_percent[i]))
-        stats["cpu"]["freq"].append(int(cpu_freq[i].current))
-        stats["cpu"]["temperature"].append(int(core_temps[i]))
-
-    # RAM (in megabytes)
-    mem = psutil.virtual_memory()
-    stats["ram"] = {
-        "total": int(mem.total / 1000**2),
-        "used": int(mem.used / 1000**2),
-        "available": int(mem.available / 1000**2)
-    }
-
-    # GPU
-    if not IGNORE_GPU:
-        gpustats = gpustat.GPUStatCollection.new_query().jsonify()
-        stats["gpu"] = {
-            "memory.used": gpustats["gpus"][0]["memory.used"],
-            "memory.total": gpustats["gpus"][0]["memory.total"],
-            "utilization": gpustats["gpus"][0]["utilization.gpu"],
-            "temperature": gpustats["gpus"][0]["temperature.gpu"]
-        }
-
+    if os.name == "nt":
+        get_cpu_info = hw_stats._get_cpu_info_wmi
     else:
-        stats["gpu"] = {
-            "memory.used": 0,
-            "memory.total": 1, # non zero value to avoid division by zero
-            "utilization": 0,
-            "temperature": 0
-        }
+        get_cpu_info = hw_stats._get_cpu_info_psutil
 
-    return stats
+    return {
+        "cpu": get_cpu_info(),
+        "ram": hw_stats.get_ram_info(),
+        "gpu": hw_stats.get_gpu_info()
+    }
 
 def publish_stats():
     """Continuously fetch current statistics and publish as message.
