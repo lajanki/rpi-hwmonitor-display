fbf98d8b54e602ea18c8cb075de2da428469858b
Lauri Ajanki <lauri.ajanki@gmail.com>
2023-07-22 16:56:32 +0300

    add service account creation and iam roles

--- setup_pubsub.sh
+++ setup_pubsub.sh
@@ -6,3 +6,18 @@ gcloud pubsub subscriptions create $SUBSCRIPTION_ID \
     --topic $TOPIC_ID \
     --project $PROJECT_ID \
     --message-retention-duration 10m
+
+# Create a service account with access to publish messages to the topic
+gcloud iam service-accounts create hwmonitor \
+    --description="hwmonitor worker identity" \
+    --project $PROJECT_ID
+
+gcloud pubsub topics add-iam-policy-binding $TOPIC_ID \
+    --member="serviceAccount:hwmonitor@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/pubsub.publisher" \
+    --project $PROJECT_ID
+
+gcloud pubsub subscriptions add-iam-policy-binding $SUBSCRIPTION_ID \
+    --member="serviceAccount:hwmonitor@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/pubsub.subscriber" \
+    --project $PROJECT_ID
