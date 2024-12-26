39f06f9f34fe8c6ee41fe2c3c1c2bb3f30f27e90
Lauri Ajanki <lauri.ajanki@gmail.com>
2024-12-26 20:40:00 +0200

    chore: mock all psutil calls

--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -6,19 +6,22 @@ from pytest_schema import schema
 from transport import hw_stats
 
 
-@patch("psutil.sensors_temperatures")
+@patch("transport.hw_stats.psutil")
 @patch("transport.hw_stats.pynvml")
-def test_get_stats_schema(mock_pynvml, mock_sensors_temperatures):
+def test_get_stats_schema(mock_pynvml, mock_psutil):
     """Validate the schema of the main hardware statistics extract function."""
     # Mock pynvml calls
     mock_pynvml.nvmlDeviceGetMemoryInfo.return_value = Mock(used=2*10**6, total=3*10**6)
     mock_pynvml.nvmlDeviceGetUtilizationRates.return_value = Mock(gpu=52)
     mock_pynvml.nvmlDeviceGetTemperature.return_value = 40
 
-    # Mock cpu temp call, this would return an empty dict on unsupported OS (ie. automated test environment).
-    mock_sensors_temperatures.return_value = {
+    # Mock psutil calls
+    mock_psutil.sensors_temperatures.return_value = {
         "coretemp": [Mock(label="#1", current=26), Mock(label="#2", current=28), Mock(label="#3", current=28)]
     }
+    mock_psutil.virtual_memory.return_value = Mock(total=5000, used=601, available=2000)
+    mock_psutil.getloadavg.return_value = [0.28]
+
 
     stats = hw_stats.get_stats()
     expected_schema = schema(
